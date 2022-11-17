4b63756b10313059695f5f0f0f9ff59e34581435
nedagosa-cod <nestorcode4004@gmail.com>
2022-11-17 12:04:50 -0500

    cambios apra base de datos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import indexRoutes from "./routes/index.js";
 import {PORT} from "./config.js"
+import {pool} from './db.js'
 
 const app = express();
 
@@ -12,5 +13,10 @@ app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(indexRoutes);
 app.use(express.static(join(__dirname, "public")));
+
+app.get("/ping", async (req, res) => {
+    const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+    res.json(result[0])
+});
 
 app.listen(PORT);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import {pool} from '../db.js'
 
 const router = Router();
 
@@ -7,8 +6,5 @@ router.get("/", (req, res) => res.render("index", {title: 'Referidos Atento'}));
 router.get("/referir", (req, res) => res.render("referir", {title: 'Referidos Atento'}));
 router.get("/estado", (req, res) => res.render("estado", {title: 'Referidos Atento'}));
 router.get("/login", (req, res) => res.render("login", {title: 'Referidos Atento'}));
-router.get("/ping", async (req, res) => {
-    const result = await pool.query(`SELECT "hello world" as RESULT`)
-});
 
 export default router;
