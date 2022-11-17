48101bb63ffd508c785919175aad159fa9dd6e93
nedagosa-cod <nestorcode4004@gmail.com>
2022-11-17 13:51:45 -0500

    configuracion base de datos local

--- src/db.js
+++ src/db.js
@@ -1,10 +1,20 @@
-import {createPool} from 'mysql2/promise'
+import mysql from 'mysql2'
 import {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} from './config.js'
 
-export const pool = createPool({
+const conn = mysql.createConnection({
     user: DB_USER,
     password: DB_PASSWORD,
     host: DB_HOST,
     port: DB_PORT,
     database: DB_NAME
 })
+
+conn.connect(function(err){
+    if (err) {
+        console.log('Error al cargar base de datos:' + err.stack);
+        return
+    }
+    console.log('conexion exitosa')
+})
+
+export {conn}
