c73c92cccfe455f82099ff8cf26bb3beee6a986f
nedagosa-cod <nestorcode4004@gmail.com>
2022-11-17 14:47:00 -0500

    se consume base de datos local 2

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_USER = process.env.DB_USER || 'nedagosa';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'nedagosapass';
-export const DB_NAME = process.env.NAME || 'users';
-export const DB_PORT = process.env.PORT || 3306;
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-118.railway.app';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'x3biFuSnXRnchFQIB4gG';
+export const DB_NAME = process.env.NAME || 'railway';
+export const DB_PORT = process.env.PORT || 5630;
